88789a885d97b886884bacd9d476794de8c96c21
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-09-29 19:13:58 +0530

    Update Data_Type.py

--- Data_Type.py
+++ Data_Type.py
@@ -1,7 +1,23 @@
 
+                                                                                  #numbers#
+
+x=10
+print(x)  #output = 10
+
+y=10.102
+print(y)  #output = 10.102
+
+z=25j
+print(z)   #output = 25j
+
+print(type(10))    #output = <class 'int'>
 
 
+print(type(10.071))    #output = <class 'float'>
 
+print(type(True))       #output = <class 'bool'>
+
+print(type(5j))         #output = <class 'complex'>
 
 
 
