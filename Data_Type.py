06b9e37ec113a132545d2952bef6d464fc875395
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-09-29 19:16:42 +0530

    Update Data_Type.py

--- Data_Type.py
+++ Data_Type.py
@@ -56,7 +56,38 @@ x
                                                                                    #string#
 
 
+#print string
+x="python"
+print(x)
+#op=python
 
+#print length of string
+len(x)
+#op=6
+
+#addition of two string
+y=" Jupyter"
+print(x+y)
+#op=python Jupyter
+
+#Upper case
+x.upper()
+#op='PYTHON'
+
+#lower case
+x.lower()
+#op='python'
+
+#position
+x[2:6]
+#op='thon'
+
+
+x[-2]
+#op='o'
+
+x[5]
+#op='n'
 
 
 
