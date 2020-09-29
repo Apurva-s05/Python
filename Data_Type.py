ef5d7452f5b31afb96bb3c8708fac71166ae6ce9
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-09-29 19:13:07 +0530

    Create Data_Type.py

---
+++ Data_Type.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                     #List#
+#Unordered,no duplicate entries present
+
+list={"lion","king","monkey",10,10,20}
+list
+#op={10, 20, 'king', 'lion', 'monkey'}
+
+range(11)
+#op=range(0, 11)
+
+set(range(11))
+#op={0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+a = 1,2,3
+b = 4,5,6
+c = [a,b]
+c
+#op=[(1, 2, 3), (4, 5, 6)]
