bdab7912eca218af38c7ea119824c50cd94a21ff
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-09-30 19:51:13 +0530

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,146 @@
+                                                                       #1.Arithmatic operator
+x = 10
+y = 20
+c
+
+#addition
+c=x+y
+#op=30
+
+#subtraction
+c=x-y
+#op=-10
+
+#multiplication
+c=x*y
+#op=200
+
+#division
+c=x/y
+#op=0.5
+
+#square
+c=x**y
+#op=100000000000000000000
+
+#flow division
+c=x//y
+#op=0
+
+#modulous
+c=x%y
+#op=10
+                                                                       #2.Assignment operator
+x = 30
+#addition
+x+=10
+x
+#op=40
+
+#subtraction
+x-=10
+x
+#op=30
+
+#multiplication
+x*=10
+x
+#op=300
+
+#division
+x/=10
+x
+#op=30.0
+
+#square
+x**=10
+x
+#op=590490000000000.0
+
+#flow division
+x//=10
+x
+#op=59049000000000.0
+
+#modulous
+x%=10
+x
+#op=0.0
+   
+                                                                        #3.Comparison Operator
+x = 30
+y = 60
+
+#equal
+c = x==y
+#op=False
+
+#not equal
+c = x!=y
+c
+#op=True
+
+#Greater than
+c = x>y
+c
+#op=False
+
+#less than
+c = x<y
+c
+#op=True
+
+#Greater than or equal
+c = x>=y
+c
+#op=False
+
+#less than or equal
+c = x<=y
+c
+#op=True
+
+x = 30
+y = 60
+
+if x == y:
+    print('Equal')
+elif x > y:
+    print('Greater')
+else: 
+    print('Smaller')
+#op=
+Smaller
+                                                                         #4.Logical Operator
+x = 30
+
+#logical and 
+x > 10 and x < 10
+#op=False
+
+#logical or 
+x > 10 or x < 10
+#op=True
+
+#logical not
+not(x > 10 and x < 10)
+#op=True 
+
+                                                                           #5.Identity Operator
+#is=Returns true if both variables are same object
+list1 = [1,2,3]
+list2 = [1,2,3]
+x = list1
+x is list1
+#op=True
+
+list1 is list2
+#op=False
+
+#is not=Returns true if both variables are not object
+
+list1 is not list2
+#op=True
+
+                                                                          #6.Membership Operator
+                                                                          
