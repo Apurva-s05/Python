737e08f758c63d1fa18d6bd0e3572dc8377144b6
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-11-15 22:47:38 +0530

    Update rectangle_Turtle.py

--- rectangle_Turtle.py
+++ rectangle_Turtle.py
@@ -3,16 +3,35 @@ import turtle
 wn = turtle.Screen()
 wn.bgcolor("orange")
 
-alex = turtle.Turtle()
-alex.color("black")
-alex.pensize(3)
+tess = turtle.Turtle()
+tess.color("black")
+tess.pensize(3)
 
-alex.forward(165)
-alex.left(90)
-alex.forward(75)
-alex.left(90)
-alex.forward(165)
-alex.left(90)
-alex.forward(75)
+tess.forward(165)
+tess.left(90)
+tess.forward(75)
+tess.left(90)
+tess.forward(165)
+tess.left(90)
+tess.forward(75)            # set his color
+
+tess.forward(80)                 # Let tess draw an equilateral triangle
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)                   # complete the triangle
+
+tess.right(180)                  # turn tess around
+tess.forward(80)                 # move her away from the origin so we can see alex
+
+tess.forward(50)                 # make alex draw a square
+tess.left(90)
+tess.forward(50)
+tess.left(90)
+tess.forward(50)
+tess.left(90)
+tess.forward(50)
+tess.left(90)
 
 wn.exitonclick()
