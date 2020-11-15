a3710173e1dda123abd763bde6c8973598909b70
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-11-15 22:11:03 +0530

    Update rectangle_Turtle.py

--- rectangle_Turtle.py
+++ rectangle_Turtle.py
@@ -1,6 +1,12 @@
 import turtle
+
 wn = turtle.Screen()
+wn.bgcolor("orange")
+
 alex = turtle.Turtle()
+alex.color("black")
+alex.pensize(3)
+
 alex.forward(165)
 alex.left(90)
 alex.forward(75)
@@ -8,3 +14,5 @@ alex.left(90)
 alex.forward(165)
 alex.left(90)
 alex.forward(75)
+
+wn.exitonclick()
