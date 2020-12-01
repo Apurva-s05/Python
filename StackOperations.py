0e709bf82fccf040f01890cb00a5b1790fb39d05
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-12-01 16:36:29 +0530

    Create StackOperations.py

---
+++ StackOperations.py
@@ -0,0 +1,19 @@
+stack = []
+
+def empty():
+    if len(stack) == 0:
+        print("Stack is empty. \n")
+    else:
+        print(stack , "\n")
+val1 = 1
+val2 = 2
+val3 = 3
+stack.append(val1)
+stack.append(val2)
+stack.append(val3)
+empty()
+a = stack.pop()
+b = stack.pop()
+c = stack.pop()
+print("poped elements are:",a,b,c)
+empty()
